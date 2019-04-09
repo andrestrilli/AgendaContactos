f8f2d47e6b1b116c123b1158eb09744d0b34eb7c
AndresTrilli-MAQ <andres_trilli@hotmail.com>
2019-04-09 02:47:37 -0500

    Escribe los datos en firebase

--- app/src/main/java/com/example/agendacontactos/CreateContact.java
+++ app/src/main/java/com/example/agendacontactos/CreateContact.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class CreateContact extends AppCompatActivity {
+    private static int IdAut=0;
     private EditText Name, LastName, Phone, CellPhone;
     private Resources Resources;
     private ArrayList<Contact> contacts;
-
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +35,7 @@ public class CreateContact extends AppCompatActivity {
 
     public void Save(View view){
         String Id, NameV, LastNameV, PhoneV, CellPhoneV;
-        Id = (contacts.size()+1)+"";
+        Id = (IdAut+"");
         NameV= Name.getText().toString();
         LastNameV = LastName.getText().toString();
         PhoneV = Phone.getText().toString();
@@ -38,7 +43,14 @@ public class CreateContact extends AppCompatActivity {
 
         Contact C = new Contact(Id,NameV,LastNameV,PhoneV,CellPhoneV);
         C.SaveContact();
+// Write a message to the database
+
+       DatabaseReference myRef = database.getReference("Contactos");
+       myRef.child(C.getId()).setValue(C);
+
         Toast.makeText(this,R.string.done,Toast.LENGTH_LONG).show();
+        //incremento el ID;
+        IdAut++;
 
     }
 
